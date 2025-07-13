b595429f6c6681ecec1e375af9985adb5ca9f37c
TrillOnGit <austinfunk99@yahoo.ca>
2025-07-13 02:12:44 -0700

    Creature Sticker Visibility

--- CardNode.cs
+++ CardNode.cs
@@ -18,6 +18,7 @@ public partial class CardNode : Area2D
     [Export] public Sprite2D CorpseSprite { get; set; } = null!;
     [Export] public Sprite2D RitualSprite { get; set; } = null!;
     [Export] public Sprite2D EyeSprite { get; set; } = null!;
+    [Export] public Sprite2D CreatureSprite { get; set; } = null!;
     [Export] public CpuParticles2D Sparkle { get; set; } = null!;
     [Export] public CardBack CardBack { get; set; } = CardBack.Red;
     [Export] public int CardRank { get; set; } = 1;
@@ -66,6 +67,7 @@ public partial class CardNode : Area2D
             CorpseSprite.Visible = data.HasSticker<CorpseSticker>();
             RitualSprite.Visible = data.HasSticker<RitualSticker>();
             EyeSprite.Visible = data.HasSticker<EyeSticker>();
+            CreatureSprite.Visible = data.HasSticker<CreatureSticker>();
         }
 
         CardBackSprite.Frame = GetBackFrame();
