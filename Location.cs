31360d82ea712fc6dccffd27f5bb3e4a2b7bb8b4
TrillOnGit <austinfunk99@yahoo.ca>
2025-07-27 23:51:17 -0700

    Location cards expanded

--- Location.cs
+++ Location.cs
@@ -11,16 +11,28 @@ public class BeastGapLocation : ILocation
     {
         //People
         CardHelpers.MakeCardData(Suit.Clubs, 1),
+        CardHelpers.MakeCardData(Suit.Clubs, 3),
+        CardHelpers.MakeCardData(Suit.Clubs, 5),
+        CardHelpers.MakeCardData(Suit.Clubs, 7),
         CardHelpers.MakeCardData(Suit.Clubs, 8),
         CardHelpers.MakeCardData(Suit.Clubs, 8),
-        CardHelpers.MakeCardData(Suit.Clubs, 10),
+        CardHelpers.MakeCardData(Suit.Clubs, 9),
+        CardHelpers.MakeCardData(Suit.Clubs, 9),
 
         //Authority
         CardHelpers.MakeCardData(Suit.Spades, 1),
-        CardHelpers.MakeCardData(Suit.Spades, 10),
+        CardHelpers.MakeCardData(Suit.Spades, 4),
+        CardHelpers.MakeCardData(Suit.Spades, 7),
 
         //Emotions
-        //CardHelpers.MakeCardData(Suit.Hearts, 8),
+        CardHelpers.MakeCardData(Suit.Hearts, 7),
+        CardHelpers.MakeCardData(Suit.Hearts, 9),
+        CardHelpers.MakeCardData(Suit.Hearts, 9),
+
+        //Items
+        CardHelpers.MakeCardData(Suit.Diamonds, 2),
+        CardHelpers.MakeCardData(Suit.Diamonds, 3),
+        CardHelpers.MakeCardData(Suit.Diamonds, 4),
     };
 }
 
